ee5bc9bddc0bd1c1bb1e1a103c9a70b2e822a321
headMade_Web <headMade@pro.ru>
2021-03-12 12:31:55 +0300

    #4299-1 feat: moved search to elements

--- components/containers/Layout/Header/Header.js
+++ components/containers/Layout/Header/Header.js
@@ -1,15 +1,9 @@
-import { useRouter } from "next/router";
-import { useTranslation } from "next-i18next";
-
 import Navigation from "./Navigation"
 import Authorization from "../../Section/Authorization";
 import LanguageChanger from "../../../elements/LanguageChanger";
-import Search from "./Search";
+import Search from "../../../elements/Search";
 
 export default function Header() {
-
-	const router = useRouter();
-	const { t } = useTranslation("common");
 
 	return (
 		<header className={"header"}>
