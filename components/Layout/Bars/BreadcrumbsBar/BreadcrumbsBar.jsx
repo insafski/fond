4e0c23f5a224bd16739c6376590e1606b3513de7
headMade_Web <headMade@pro.ru>
2021-03-17 13:08:22 +0300

    #4356 feat: add breadcrumbs

---
+++ components/Layout/Bars/BreadcrumbsBar/BreadcrumbsBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Breadcrumbs from "@/components/Layout/Breadcrumbs";
+
+export default function BreadcrumbsBar() {
+	return (
+		<div className="breaadcrumb-bar">
+			<div className="container py-4">
+				<Breadcrumbs />
+			</div>
+		</div>
+	);
+}
--- components/Layout/Breadcrumbs/Breadcrumbs.jsx
+++ components/Layout/Breadcrumbs/Breadcrumbs.jsx
@@ -1,3 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 export default function Breadcrumbs() {
-	return null;
+	const router = useRouter();
+	const [breadcrumbs, setBreadcrumbs] = useState(null);
+
+	function convertBreadcrumb(string) {
+		return string
+			.replace(/-/g, " ")
+			.replace(/oe/g, "ö")
+			.replace(/ae/g, "ä")
+			.replace(/ue/g, "ü")
+			.toUpperCase();
+	}
+
+	useEffect(() => {
+		if (router) {
+			const linkPath = router.asPath.split("/");
+
+			linkPath.shift();
+
+			const pathArray = linkPath.map((path, i) => {
+				return {
+					breadcrumb: path,
+					href: `/${linkPath.slice(0, i + 1).join("/")}`,
+				};
+			});
+
+			setBreadcrumbs(pathArray);
+		}
+	}, [router]);
+
+	return !!breadcrumbs && (
+		<nav aria-label={"breadcrumbs"} className={"breadcrumbs"}>
+			<ul className={"breadcrumbs__list flex"}>
+				<li className={"breadcrumbs__item mr-4"}>
+					<Link href={"/"}>
+						<a>Главная</a>
+					</Link>
+				</li>
+				{
+					breadcrumbs.map(({ href, breadcrumb }, idx) => {
+						return (
+							<li key={`${href}-${idx}`} className={"breadcrumbs__item mr-4"}>
+								<Link href={href}>
+									<a className={"breadcrumbs__link"}>
+										{convertBreadcrumb(breadcrumb)}
+									</a>
+								</Link>
+							</li>
+						);
+					})
+				}
+			</ul>
+		</nav>
+	);
 }
