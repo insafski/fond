86e56c63bb13ec8a8f7151d10f61b3c6dcd73a0a
Dmitriy Konovalov <dimusig@gmail.com>
2021-03-24 11:08:23 +0300

    #4360 feat:test configs integrated

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handle = app.getRequestHandler();
 
 (async () => {
-    await app.prepare();
-    const server = express();
+	await app.prepare();
+	const server = express();
 
-    server.use(nextI18NextMiddleware(nextI18next));
+	server.use(nextI18NextMiddleware(nextI18next));
 
-    server.get("*", (req, res) => handle(req, res));
+	server.get("*", (req, res) => handle(req, res));
 
-    await server.listen(port);
-    console.log(`> Ready on http://localhost:${port}`);
+	await server.listen(port);
+	console.log(`> Ready on http://localhost:${port}`);
 })();
