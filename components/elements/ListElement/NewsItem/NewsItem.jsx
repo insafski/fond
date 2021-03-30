fa8b55c0c969c68b93c5e8d1a9c47ee57de4c73e
Insaf Almukhametov <insaf_13@mail.ru>
2021-03-30 16:05:25 +0300

    fix: new sitem hover effect

--- components/elements/ListElement/NewsItem/NewsItem.jsx
+++ components/elements/ListElement/NewsItem/NewsItem.jsx
@@ -1,47 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
 import get from "lodash/get";
+import Link from "next/link";
 
 import Picture from "@/components/elements/Picture";
 
-export default function NewsItem({ heading, categories, country, date, picture }) {
+export default function NewsItem({ heading, slug, categories, country, date, picture }) {
 	const title = get(heading, "title", "");
 	const description = get(heading, "description", "");
 
+	console.log(slug);
+
 	return (
-		<div className={"news-item flex-col w-full md:w-2/6"}>
-			<div className={"news-item__picture mb-1"}>
-				<Picture items={picture} />
+		<Link href={`/news/${slug}`}>
+			<div className={"news-item flex-col w-full md:w-2/6 hover:bg-green-100 cursor-pointer"}>
+				<div className={"news-item__picture mb-1"}>
+					<Picture items={picture} />
+				</div>
+				<div className={"news-item__meta flex justify-between mb-2"}>
+					<span className={"country"}>
+						{country}
+					</span>
+					<span className={"date"}>
+						{date}
+					</span>
+				</div>
+				<div className={"news-item__title mb-0.5"}>
+					<h3>
+						{title}
+					</h3>
+				</div>
+				<div className={"news-item__description mb-1"}>
+					<p>
+						{description}
+					</p>
+				</div>
+				<div className={"news-item__categories"}>
+					<span className={"font-bold border-b-4 border-yellow-900 py-2"}>
+						{categories}
+					</span>
+				</div>
 			</div>
-			<div className={"news-item__meta flex justify-between mb-2"}>
-				<span className={"country"}>
-					{country}
-				</span>
-				<span className={"date"}>
-					{date}
-				</span>
-			</div>
-			<div className={"news-item__title mb-0.5"}>
-				<h3>
-					{title}
-				</h3>
-			</div>
-			<div className={"news-item__description mb-1"}>
-				<p>
-					{description}
-				</p>
-			</div>
-			<div className={"news-item__categories"}>
-				<span className={"font-bold border-b-4 border-yellow-900 py-2"}>
-					{categories}
-				</span>
-			</div>
-		</div>
+		</Link>
 	);
 }
 
 NewsItem.propTypes = {
 	heading: PropTypes.object,
+	slug: PropTypes.string,
 	categories: PropTypes.string,
 	country: PropTypes.string,
 	date: PropTypes.string,
@@ -51,6 +57,7 @@ NewsItem.propTypes = {
 
 NewsItem.defaultProps = {
 	heading: {},
+	slug: "",
 	categories: "",
 	country: "",
 	date: "",
